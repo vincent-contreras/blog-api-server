ddfaf623174621d1ed0dfcd25b2d27da662703ad
Paul Vincent Contreras <vincent.contreras@outlook.com>
2024-12-05 01:17:35 +0900

    Add login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :categories
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #
+  scope "/auth" do
+    delete "/", to: "auth#destroy", as: :logout
+    post "login", to: "auth#login", as: :login
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
